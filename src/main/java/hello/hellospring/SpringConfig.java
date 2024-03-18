baa041615adab18eac9032f656f276ca1d3dc24e
박하민 <parkhamint@naver.com>
2024-03-19 01:39:55 +0900

    AOP

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,12 @@ public class SpringConfig {
     public MemberService memberService(){
         return new MemberService(memberRepository);
     }
+
+    /*@Bean
+    public TimeTraceAop timeTraceAop(){
+        return new TimeTraceAop();
+    bean 등록해도 되지만 TimeTraceAop에 직접 Component annotation 달아줌.
+    }*/
 
     /*@Bean
     public MemberRepository memberRepository(){
